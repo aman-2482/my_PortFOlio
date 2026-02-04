52c464e3595166a84d2fae109414ef8e8b17a32b
Aman chaudhary <122973109+aman-2482@users.noreply.github.com>
2026-02-04 14:51:59 +0530

    Clean up in timeout function
    Removed unnecessary comments from the timeout function.

--- flutter.js
+++ flutter.js
@@ -27,10 +27,7 @@ _flutter.loader = null;
    *
    * Resolves/rejects with whatever the original `promises` does, or rejects
    * if `promise` takes longer to complete than `duration`. In that case,
-   * `debugName` is used to compose a legible error message.
-   *
-   * If `duration` is < 0, the original `promise` is returned unchanged.
-   *
+   * `debugName` is used to compose a legible error mes
    */
   async function timeout(promise, duration, debugName) {
     if (duration < 0) {
